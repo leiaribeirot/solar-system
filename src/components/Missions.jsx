9dfa6272ab0696d012efe98520f513375b6619af
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-29 14:57:11 -0400

    Requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 function Missions() {
   return (
     <div data-testid="missions">
       <Title headline="Missões" />
+      {missions.map(({ name, year, country, destination }) => (
+        <MissionCard
+          key={ name }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      ))}
     </div>
   );
 }
