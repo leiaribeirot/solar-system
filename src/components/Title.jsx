2b9a5c0c76aa45bd435f7ea815efe47fd891a474
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-29 12:42:28 -0400

    Requisitos 3 e 4 OK

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Title extends Component {
-  render() {
-    const { headline } = this.props;
-    return <h2>{headline}</h2>;
-  }
+function Title({ headline }) {
+  return (
+    <h2>{headline}</h2>
+  );
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
+
+export default Title;
