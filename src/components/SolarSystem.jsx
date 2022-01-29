d1c2bf73f993c60a8e12f445d5d5987a4df5933e
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-29 13:24:27 -0400

    Requisitos 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
@@ -7,6 +9,9 @@ export default class SolarSystem extends Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        { Planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+        )) }
       </>
     );
   }
